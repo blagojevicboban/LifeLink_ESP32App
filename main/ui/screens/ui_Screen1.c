e8d9630d41515e24551895237f581a4db4eacf42
Boban Blagojević <boban.blagojevic@tsp.edu.rs>
2026-01-17 11:04:22 +0100

    back color

--- main/ui/screens/ui_Screen1.c
+++ main/ui/screens/ui_Screen1.c
@@ -16,6 +16,8 @@ void ui_Screen1_screen_init(void)
 {
     ui_Screen1 = lv_obj_create(NULL);
     lv_obj_clear_flag(ui_Screen1, LV_OBJ_FLAG_SCROLLABLE);      /// Flags
+    lv_obj_set_style_bg_color(ui_Screen1, lv_color_hex(0x001220), LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_bg_opa(ui_Screen1, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
 
     ui_Button1 = lv_btn_create(ui_Screen1);
     lv_obj_set_width(ui_Button1, 100);
